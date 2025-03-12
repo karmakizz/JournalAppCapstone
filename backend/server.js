fb588c966d843e861dbe919a2e7c63d70a28d741
Iris Lopez <iris.lopez.co@gmail.com>
2025-03-12 14:01:34 -0600

    working on backend setting up the server and making sure mongo db is connected

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 7777;
+
+// Middleware
+app.use(cors());
+app.use(express.json());  // to parse incoming JSON requests
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
