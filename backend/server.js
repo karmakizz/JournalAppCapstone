4ae2811520684b085948065d021cc05e8e5627ee
Iris Lopez <iris.lopez.co@gmail.com>
2025-03-12 14:09:22 -0600

    successfully set up your backend and frontend to communicate with each other.

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ const app = express();
 const port = process.env.PORT || 7777;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',  // Allow only this origin
+  }));
 app.use(express.json());  // to parse incoming JSON requests
 
 // Connect to MongoDB
