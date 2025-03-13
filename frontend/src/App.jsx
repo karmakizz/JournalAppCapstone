b8f2e194dcfba68389ed7b0819c8e646d7ed4685
Iris Lopez <iris.lopez.co@gmail.com>
2025-03-13 09:56:49 -0600

    moving some components to the right place

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import JournalEntries from './components/JournalEntries';  // Your new component
+import React,{useState} from 'react';
+import JournalEntries from './pages/JournalEntries';  // Your new component
+import Login from './components/Login';
 
 function App() {
+  const [username, setUsername] = useState(null);
+  
   return (
     <div className="App">
-      <JournalEntries />  {/* This will display the list of journals */}
+      {!username ? (
+        <Login onLogin={setUsername} />
+      ) : (
+        <div>
+          <h1>Welcome, {username}!</h1>
+          <JournalEntries />
+        </div>
+      )}
     </div>
   );
 }
-
 export default App;
